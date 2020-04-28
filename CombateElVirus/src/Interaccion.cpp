c7aefc234e8a98a1fa8d05161f2a3b6da5f8a51e
99anaduegor <99anaduegor@gmail.com>
2020-04-28 09:19:42 +0200

    mas limpieza

--- CombateElVirus/src/Interaccion.cpp
+++ CombateElVirus/src/Interaccion.cpp
@@ -80,25 +80,6 @@ void Interaccion::reboteexterior(Hombre& h,Limites c)
       }
       return false;
  }
-/*
-bool Interaccion::rebote(Esfera& e, Pared p)
-{
-    Vector2D dir;
-    float dif = p.distancia(e.posicion, &dir) - e.radio;
-    if (dif <= 0.0f)
-    {
-        Vector2D v_inicial = e.velocidad;
-        e.velocidad = v_inicial - dir * 2.0 * (v_inicial * dir);
-        e.posicion = e.posicion - dir * dif;
-        return true;
-    }
-    return false;
-}
 
-bool Interaccion::rebote(Esfera& e, Caja c) //CUTREEEEEEEEEEEEEEEEEEEEEEEEE
-{
-    return false;
-}
-*/
 
 
